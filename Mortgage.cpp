b19b568fd855d2e1e0852b314d3126adca70ee1e
Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>
2022-10-09 09:47:13 -0400

    Added extra tab to payments past 1000.
    I forgot that tabs can be configured to be of different sizes. This change matches the spec provided by my professor.
    
    Signed-off-by: Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>

--- Mortgage.cpp
+++ Mortgage.cpp
@@ -154,10 +154,7 @@ void writeRow(ofstream &outfile, double principle, double interest, double remai
 	{
 		row = "$" + row;
 	}
-	if (payment < 1000) {
-		// Payments past 1000 do not require an additional tab
-		row = "\t" + row;
-	}
+	row = "\t" + row;
 
 	row = to_string(payment) + row + "\n";
 
